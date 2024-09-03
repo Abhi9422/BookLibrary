7cb421b0b50453ca13ead5122bfadb6690947829
Abhijit Anarase <126268955+Abhi9422@users.noreply.github.com>
2024-09-03 13:35:48 +0530

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -10,7 +10,7 @@ const ShopContextProvider = ({ children }) => {
   const[bookId , setBookId] = useLocalStorage("bookId", "");
   const [data, setData] = useState(null);
   const [author, setAuthor] = useLocalStorage("author", {});
-  const url = "http://localhost:4000";
+  const url = "https://book-library-qwvo.onrender.com";
   useEffect(() => {
     axios
       .get(`${url}/api/books/list`)
